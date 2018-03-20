08e9a67d9232439dde79f7d2190f2d937f324c46
Sam Watts <samuel.watts@gmail.com>
2018-03-20 13:49:52 +0000

    initial commit

---
+++ CurveChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurveChallenge
+//
+//  Created by Sam Watts on 20/03/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
